89577c624e6ba371d0a5caba532e179ab0eb25ca
brandiherrera <bcjherrera@gmail.com>
2020-01-24 21:13:59 -0800

    fixed migrations and seed data

--- seeds/seed.museum_app_data.sql
+++ seeds/seed.museum_app_data.sql
@@ -15,9 +15,12 @@ INSERT INTO museum_art_data (object_id, primary_image, art_title, art_artist, ar
     (300, 'https://images.metmuseum.org/CRDImages/ad/original/69178.jpg', 'Balcony', '', '1800-1830'),
     (4000, 'https://images.metmuseum.org/CRDImages/ad/original/112937.jpg', 'Fragment', '', '1700-1800'),
     (436535, 'https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg', 'Wheat Field with Cypresses', 'Vincent van Gogh', '1889'),
+    -- (436, 'https://images.metmuseum.org/CRDImages/ep/original/DT1567.jpg', 'Wheat Field with Cypresses', 'Vincent van Gogh', '1889'),
     (438012, 'https://images.metmuseum.org/CRDImages/ep/original/DT1877.jpg', 'Bouquet of Chrysanthemums', 'Auguste Renoir', '1881');
 
 INSERT INTO users_comments (art_id, user_id, comment) VALUES
     (436535, 2, 'This is fantastic! I should put it in my office at Dunder Mifflin!!'),
     (436535, 3, 'I agree, Michael. You should.'),
+    -- (436, 2, 'This is fantastic! I should put it in my office at Dunder Mifflin!!'),
+    -- (436, 3, 'I agree, Michael. You should.'),
     (438012, 4, 'So beautiful, I love art so much <3');
