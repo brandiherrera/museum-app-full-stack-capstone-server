7b7a73cbb181d3d29ab90a9015a9b3690c262a74
brandiherrera <bcjherrera@gmail.com>
2020-01-28 13:24:53 -0800

    updated art-service and art-router with getById

--- src/art/art-router.js
+++ src/art/art-router.js
@@ -6,6 +6,15 @@ const ArtService = require('./art-service')
 const artRouter = express.Router()
 const jsonParser = express.json()
 
+// const serializeArt = art => ({
+//     id: art.id,
+//     object_id: art.object_id,
+//     primary_image: art.primary_image,
+//     art_title: art.art_title,
+//     art_artist: art.art_artist,
+//     art_date: art.art_date,
+// })
+
 artRouter
     .route('/')
     .get((req, res, next) => {
@@ -14,6 +23,25 @@ artRouter
                 res.json(art)
             })
             .catch(next)
+    })
+
+artRouter
+    .route('/:object_id')
+    .all((req, res, next) => {
+        ArtService.getById(req.app.get('db'), req.params.object_id)
+            .then(art => {
+                if (!art) {
+                    return res.status(404).json({
+                        error: { message: `Art information doesn't exist` }
+                    })
+                }
+                res.art = art
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(res.art)
     })
 
 module.exports = artRouter
--- src/art/art-service.js
+++ src/art/art-service.js
@@ -1,7 +1,14 @@
 const ArtService = {
     getArtGallery(knex) {
         return knex.select('*').from('museum_art_data')
-    }
+    },
+    getById(knex, object_id) {
+        return knex
+            .from('museum_art_data')
+            .select('*')
+            .where('object_id', object_id)
+            .first()
+    },
 }
 
 module.exports = ArtService
