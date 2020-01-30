0d2aedb1e0a2d25c93b8df0f6feeb54a167c4e7b
brandiherrera <bcjherrera@gmail.com>
2020-01-30 14:01:30 -0800

    prepare initial deploy

--- src/art/art-service.js
+++ src/art/art-service.js
@@ -11,11 +11,32 @@ const ArtService = {
             .where('object_id', object_id)
             .first()
     },
+    // getUserGallery(knex, user_id) {
+    //     return knex
+    //         .from('users_gallery')
+    //         .select('*')
+    //         .where('user_id', user_id)
+    // },
     getUserGallery(knex, user_id) {
         return knex
-            .from('users_gallery')
-            .select('*')
+            .from('users_gallery AS user'/*, 'museum_users AS user'*/)
             .where('user_id', user_id)
+            .select('*')
+            // .select(
+            //     'user.art_id',
+            //     'user.user_id'
+            // )
+            .join(
+                'museum_art_data AS art',
+                'user.art_id',
+                'art.object_id'
+                )
+                .returning('*')
+                // .then(rows => {
+                //     return rows[0]
+                // })
+            // .using('users_gallery', 'museum_users')
+            // .where('users_gallery.user_id', user_id = 'museum_users.id')
     },
     insertGalleryItem(knex, newGalleryItem) {
         return knex
